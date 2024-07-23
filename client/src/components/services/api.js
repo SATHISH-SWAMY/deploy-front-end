663fd1f759f1dbc2d380aee9cf01b6693789f294
Sathish Swamy <145578184+SATHISH-SWAMY@users.noreply.github.com>
2024-07-23 20:25:37 +0530

    Update api.js

--- client/src/components/services/api.js
+++ client/src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://backend-code-qx1r.onrender.com';
 
 const API_GMAIL = async (urlObject, payload, type) =>{
     return await axios({
